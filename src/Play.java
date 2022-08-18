94052b3412e349da9c4684344eafaff52e787ac3
LG Gram <LG@Gram>
2022-08-18 16:04:31 -0400

    first commit

---
+++ src/Play.java
@@ -0,0 +1,7 @@
+public class Play {
+    public static void main(String[] args) {
+        System.out.println("Play starts here");
+        IConsole play1 = new SportsGame();
+        play1.move();
+    }
+}
---
+++ src/SportsGame.java
@@ -0,0 +1,7 @@
+public class SportsGame implements IConsole {
+
+    @Override
+    public void move() {
+        System.out.println("moving");
+    }
+}
